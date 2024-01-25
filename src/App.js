21486e21bab9bf73f810cb23f5ff459d32b9dfe7
Rodolfo <carmen.rodolfo2003@gmail.com>
2024-01-25 00:36:52 -0600

    Country Displayed working but increment gold does not

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ import Country from './components/Country';
 
 
 class App extends Component {
+  state = {
+    countries: [
+      { id: 1, name: 'United States', gold: 2 },
+      { id: 2, name: 'China', gold: 3 },
+      { id: 3, name: 'Germany', gold: 0 },
+    ]    
+  }
+  IncrementGold = (countryId) => {
+    
+    console.log(`IncrementGold: ${countryId}` );
+  }
   render() { 
     return ( 
       <div className="App">
-        <Country />
+        { this.state.countries.map( country => 
+        <Country 
+        key={ country.id}
+        id={ country.id }
+        onIncremnt={ this.IncrementGold}
+        name={country.name}
+        gold={ country.gold}
+         />)}
       </div>
      );
   }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,8 +2,8 @@ import React, {Component} from "react";
 
 class Country extends Component {
     state = {
-        name: "United States",
-        gold: 0
+        name: this.props.name,
+        gold: this.props.gold,
     }
     handleIncrement = () => {
         this.setState(
@@ -11,11 +11,12 @@ class Country extends Component {
         );
     }
     render(){
+        console.log(this.props);
         return (
             <div>
                 <div className="CountryName" > {this.state.name}</div>
                 <div>Gold Medals: {this.state.gold} 
-                <button variant="contained" className="incrementButton" onClick={ this.handleIncrement}> + </button>
+                <button variant="contained" className="incrementButton" onClick={ () => this.props.onIncremnt(this.props.id)}> + </button>
                 </div>
             </div>
         )
